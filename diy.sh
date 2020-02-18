be5531efbcb4fcd8ca3bb890d1001c1a07d14666
yifengyou <842056007@qq.com>
2020-02-18 13:21:28 +0800

    modify yml & modify diy.sh

--- diy.sh
+++ diy.sh
@@ -14,7 +14,7 @@
 sed -i 's/OpenWrt/YOU/g' package/base-files/files/bin/config_generate
 
 #echo '修改网关地址'
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 #echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
